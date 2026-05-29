b1fde425e8f4c6071c879162eb6219e972caf98e
FladYannic <flad.yannic@gmail.com>
2026-05-29 11:13:11 +0200

    style(frontend): easy localhost change with boolean constant

--- frontend/webapp/src/config.tsx
+++ frontend/webapp/src/config.tsx
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'http://193.196.36.46:8000'; // The URL from which the information is fetched. Ensure keeping this up to date!
+const isLocal = false;
+
+export const API_BASE_URL = isLocal
+  ? 'http://localhost:4000'
+  : 'http://193.196.36.46:8000'; // The URL from which the information is fetched. Ensure keeping this up to date!
